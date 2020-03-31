3ff92defaa160e0b04721787777ea91fc4e29801
haohonglong <947923503@qq.com>
2020-03-31 19:44:31 +0800

    modify

--- mysql.sh
+++ mysql.sh
@@ -1,21 +1,9 @@
 #!/bin/bash
 
 
-
-#root用户密码
-ROOT_PASSWORD=123456
-#php版本,因为php版本间配置文件模板不相同，此处的版本号只能为大于7.0以上版本
-PHP_VER=7.2.8
-#nginx版本
-NGINX_VER=1.15.2
 #mysql版本
 MYSQL_VER=5.7.23
-#redis版本
-REDIS_VER=4.0.11
-#redis密码
-REDIS_PASS=123456
-#phpmyadmin版本
-PHPMYADMIN_VER=4.7.6
+MYSQL_PASSWORD="123456"
 #mysql data目录
 MYSQL_DATA_DIR="/data/mysql"
 #mysql pid目录
@@ -24,26 +12,6 @@ MYSQL_PID_DIR="/var/run/mysql"
 MYSQL_LOG_DIR="/var/log/mysql"
 #mysql sock目录
 MYSQL_SOCK_DIR="/var/lib/mysql"
-#xhprof版本
-XHPROF_VER="1.2"
-#hiredis版本
-HIREDIS_VER="0.13.3"
-#swoole版本
-SWOOLE_VER="4.0.4"
-#go版本
-GO_VER="1.10.3"
-#node.js版本
-NODE_VER="8.11.4"
-#mongodb版本
-MONGODB_VER="4.0.1"
-#mongodb data目录
-MONGODB_DATA_DIR="/data/mongodb"
-#java版本
-JDK_VER="1.8"
-#maven版本
-MAVEN_VER="3.6.0"
-
-
 
 
 #安装mysql
